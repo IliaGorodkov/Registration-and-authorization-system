8550b40a92a932b7657e77f7d5064a40d72ebf09
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2020-05-28 23:42:21 +0400

    Add files via upload

--- www.webdevtest.com/index.php
+++ www.webdevtest.com/index.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href ="css/style.css">
 </head>
 <body>
-  <div class = "container mt-4" ><br>
+  <div class = "container mt-1" ><br>
   <?php
    if ($_COOKIE['user'] == ''):
   ?>
@@ -17,17 +17,18 @@
    <font face="Helvetica"> 
    <font size="+3" style= "color: green">Форма Регистрации</font>
     <DIV STYLE="font-size:15px; font-weight: bold">
-     <form action="validation-form/check.php" method = "post"><br>
+    <form action="validation-form/check.php" method = "post"><br>
      <input type = "text" class = "form-control"name= "login" id= "login" placeholder="Введите Логин"><br>
-     <input type = "text" class = "form-control"name = "name" id= "name" placeholder="Введите Имя" ><br>
-     <input type = "password" class ="form-control"name = "pass" id= "pass" placeholder="Введите Пароль"><br>
+     <input type = "text" class = "form-control"name= "email" id= "email" placeholder="Введите ваш Email"><br>
+     <input type = "password" class = "form-control"name = "pass" id= "pass" placeholder="Введите Пароль" ><br>
+     <input type = "password2" class ="form-control"name = "pass" id= "pass2" placeholder="Введите еще раз Пароль"><br>
      <button class ="btn btn-success" type="submit"> Зарегестрировать</button>
    </form><br>
 </div>
 <div class = "col">
   <font size="+3" style= "color: green">Форма Авторизации</font>
     <form action="validation-form/auth.php" method = "post"><br>
-   <input type = "text" class = "form-control"name= "login" id= "login" placeholder="Введите Логин"><br>
+   <input type = "text" class = "form-control"name= "email" id= "email" placeholder="Введите Email"><br>
    <input type = "password" class ="form-control"name = "pass" id= "pass" placeholder="И Пароль"><br>
    <button class ="btn btn-success" type="submit"> Авторизоваться </button>
   </form>
--- www.webdevtest.com/validation-form/auth.php
+++ www.webdevtest.com/validation-form/auth.php
@@ -1,21 +1,29 @@
 <?php
-$login=filter_var(trim ($_POST['login']),
+$email=filter_var(trim ($_POST['email']),
 FILTER_SANITIZE_STRING);
 $pass=filter_var(trim ($_POST['pass']),
 FILTER_SANITIZE_STRING);
 
 $pass = md5($pass."qwerty234"); 
 
-$mysql = new mysqli ('localhost','qwer','','qwer');
+$db_host = "localhost"; 
+$db_user = "root"; // Логин БД
+$db_password = "mysql"; // Пароль БД
+$db_base = 'kukold'; // Имя БД
+$db_table = "users"; 
 
-$result = $mysql -> query("SELECT * FROM `users`WHERE `login`= '$login' AND `pass`= '$pass'");
+$mysqli = mysqli_connect($db_host,$db_user,$db_password,$db_base);
 
-$users = $result->fetch_assoc();
+$result = mysqli_query("SELECT * FROM `users`WHERE `email`= '$email' AND `pass`= '$pass'");
+
+$user = $result->fetch_assoc();
 
 if(@count ($users) == 0 ) {
   echo "Такой Пользователь не найден";
   exit();
 }
+print_r($users);
+exit();
 
 setcookie ('user',$users['name'], time()+3600,"/" );
 
